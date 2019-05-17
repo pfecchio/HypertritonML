8af08bbc358ed1e118e34c7e465e9661308ef2fc
Francesco Mazzaschi <francesco.mazzaschi@cern.ch>
2019-05-17 18:37:56 +0200

    Remove rejection and add reweighting in [10,30] and [30,40]

--- Hypertriton2Body/TreeGeneration/HyperTreeFatherData.cc
+++ Hypertriton2Body/TreeGeneration/HyperTreeFatherData.cc
@@ -24,30 +24,35 @@ float SProd(TLorentzVector a1, TLorentzVector a2) { return fabs(a1[0] * a2[0] + 
 
 float Sq(float a) { return a * a; }
 
-float VProd(TLorentzVector a1, TLorentzVector a2) {
+float VProd(TLorentzVector a1, TLorentzVector a2)
+{
   float x = a1[1] * a2[2] - a2[1] * a1[2];
   float y = a1[2] * a2[0] - a1[0] * a2[2];
   float z = a1[0] * a2[1] - a1[1] * a2[0];
   return TMath::Sqrt(x * x + y * y + z * z);
 }
 
-template <typename F> double Hypot(F a, F b) { return std::sqrt(a * a + b * b); }
+template <typename F>
+double Hypot(F a, F b) { return std::sqrt(a * a + b * b); }
 
-template <typename F> double Hypot(F a, F b, F c) { return std::sqrt(a * a + b * b + c * c); }
+template <typename F>
+double Hypot(F a, F b, F c) { return std::sqrt(a * a + b * b + c * c); }
 
-template <typename F> double Hypot(F a, F b, F c, F d) { return std::sqrt(a * a + b * b + c * c + d * d); }
+template <typename F>
+double Hypot(F a, F b, F c, F d) { return std::sqrt(a * a + b * b + c * c + d * d); }
 
-void HyperTreeFatherData() {
-  TFile *myFile         = TFile::Open("~/data/hyper2body_data/HyperTritonTree_18r.root", "r");
+void HyperTreeFatherData()
+{
+  TFile *myFile = TFile::Open("~/HypertritonData/HyperTritonTree_18r.root", "r");
   TDirectoryFile *mydir = (TDirectoryFile *)myFile->Get("_custom");
   TTreeReader fReader("fTreeV0", mydir);
   TTreeReaderArray<RHyperTritonHe3pi> RHyperVec = {fReader, "RHyperTriton"};
-  TTreeReaderValue<RCollision> RColl            = {fReader, "RCollision"};
+  TTreeReaderValue<RCollision> RColl = {fReader, "RCollision"};
 
   // TH1D *fHistCent = new TH1D("fHistCent", "", 1000, 0, 100);
   // fHistCent->SetDirectory(0);
 
-  TFile tfileHist("~/data/hyper2body_data/CentHist.root", "READ");
+  TFile tfileHist("CentHist.root", "READ");
   TH1D *fHistCent = (TH1D *)tfileHist.Get("fHistCent");
   fHistCent->SetDirectory(0);
   tfileHist.Close();
@@ -55,7 +60,7 @@ void HyperTreeFatherData() {
   double fMin = (double)fHistCent->GetBinContent(280);
   cout << fMin << endl;
 
-  TFile tfile("~/data/hyper2body_data/HyperTree_Data.root", "RECREATE");
+  TFile tfile("~/HypertritonData/HyperTree_Data.root", "RECREATE");
   TTree *tree = new TTree("HyperTree_Data", "An example of a ROOT tree");
 
   int Nev1040 = 0;
@@ -94,21 +99,22 @@ void HyperTreeFatherData() {
   tree->Branch("Lrec", &Lrec);
   tree->Branch("Centrality", &Centrality);
 
-  while (fReader.Next()) {
+  while (fReader.Next())
+  {
     Centrality = RColl->fCent;
-    if (Centrality < 10.051 || Centrality > 40.05) continue;
-    for (int i = 0; i < (static_cast<int>(RHyperVec.GetSize())); i++) {
+    if (Centrality < 10.051 || Centrality > 40.05)
+      continue;
+    int bin = (int)(Centrality * 10.);
+    double height = (double)fHistCent->GetBinContent(bin);
+  //  if ((gRandom->Rndm() * height) > fMin)
+  //    continue;
 
-      int bin = (int)(Centrality * 10.);
-      double height = (double)fHistCent->GetBinContent(bin);
-
-      if ((gRandom->Rndm() * height) > fMin) continue;
-
-      Nev1040++;
-
+    Nev1040++;
+    for (int i = 0; i < (static_cast<int>(RHyperVec.GetSize())); i++)
+    {
       auto RHyper = RHyperVec[i];
       double eHe3 = Hypot(RHyper.fPxHe3, RHyper.fPyHe3, RHyper.fPzHe3, AliPID::ParticleMass(AliPID::kHe3));
-      double ePi  = Hypot(RHyper.fPxPi, RHyper.fPyPi, RHyper.fPzPi, AliPID::ParticleMass(AliPID::kPion));
+      double ePi = Hypot(RHyper.fPxPi, RHyper.fPyPi, RHyper.fPzPi, AliPID::ParticleMass(AliPID::kPion));
 
       TLorentzVector he3Vector, piVector, hyperVector;
       he3Vector.SetPxPyPzE(RHyper.fPxHe3, RHyper.fPyHe3, RHyper.fPzHe3, eHe3);
@@ -117,35 +123,38 @@ void HyperTreeFatherData() {
 
       TVector3 v(RHyper.fDecayX, RHyper.fDecayY, RHyper.fDecayZ);
       float pointAngle = hyperVector.Angle(v);
-      float CosPA      = std::cos(pointAngle);
+      float CosPA = std::cos(pointAngle);
 
       float qP, qN, qT;
-      if (RHyper.fMatter == true) {
+      if (RHyper.fMatter == true)
+      {
         qP = SProd(hyperVector, he3Vector) / fabs(hyperVector.P());
         qN = SProd(hyperVector, piVector) / fabs(hyperVector.P());
         qT = VProd(hyperVector, he3Vector) / fabs(hyperVector.P());
-      } else {
+      }
+      else
+      {
         qN = SProd(hyperVector, he3Vector) / fabs(hyperVector.P());
         qP = SProd(hyperVector, piVector) / fabs(hyperVector.P());
         qT = VProd(hyperVector, piVector) / fabs(hyperVector.P());
       }
 
-      float alpha     = (qP - qN) / (qP + qN);
-      DistOverP       = Hypot(RHyper.fDecayX, RHyper.fDecayY, RHyper.fDecayZ) / hyperVector.P();
-      InvMass         = hyperVector.M();
+      float alpha = (qP - qN) / (qP + qN);
+      DistOverP = Hypot(RHyper.fDecayX, RHyper.fDecayY, RHyper.fDecayZ) / hyperVector.P();
+      InvMass = hyperVector.M();
       ArmenterosAlpha = alpha;
-      V0CosPA         = CosPA;
-      V0Chi2          = RHyper.fChi2V0;
-      PiProngPt       = Hypot(RHyper.fPxPi, RHyper.fPyPi);
-      He3ProngPt      = Hypot(RHyper.fPxHe3, RHyper.fPyHe3);
-      ProngsDCA       = RHyper.fDcaV0daughters;
-      PiProngPvDCA    = RHyper.fDcaPi2PrimaryVertex;
-      He3ProngPvDCA   = RHyper.fDcaHe32PrimaryVertex;
-      Lrec            = Hypot(RHyper.fDecayX, RHyper.fDecayY, RHyper.fDecayZ);
+      V0CosPA = CosPA;
+      V0Chi2 = RHyper.fChi2V0;
+      PiProngPt = Hypot(RHyper.fPxPi, RHyper.fPyPi);
+      He3ProngPt = Hypot(RHyper.fPxHe3, RHyper.fPyHe3);
+      ProngsDCA = RHyper.fDcaV0daughters;
+      PiProngPvDCA = RHyper.fDcaPi2PrimaryVertex;
+      He3ProngPvDCA = RHyper.fDcaHe32PrimaryVertex;
+      Lrec = Hypot(RHyper.fDecayX, RHyper.fDecayY, RHyper.fDecayZ);
       NpidClustersHe3 = RHyper.fNpidClustersHe3;
-      TPCnSigmaPi     = RHyper.fTPCnSigmaPi;
-      TPCnSigmaHe3    = RHyper.fTPCnSigmaHe3;
-      V0pt            = hyperVector.Pt();
+      TPCnSigmaPi = RHyper.fTPCnSigmaPi;
+      TPCnSigmaHe3 = RHyper.fTPCnSigmaHe3;
+      V0pt = hyperVector.Pt();
       tree->Fill();
     }
   }
