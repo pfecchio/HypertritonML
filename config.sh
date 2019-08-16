dc63c8541fe96432b4db6ec5daf0ef6d07716b0d
Pietro Fecchio <pfecchio@cern.ch>
2019-08-16 18:45:43 +0200

    introduce new environment variables schema in config.sh

--- config.sh
+++ config.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-export HYPERML_DATA="$HOME/HypertritonAnalysis/Trees"
-export HYPERML_TABLES="$HOME/HypertritonAnalysis/Tables"
-export HYPERML_UTILS="$HOME/HypertritonAnalysis/Utils"
+HYPERML_DATA="$HOME/HypertritonAnalysis/Trees"
+HYPERML_TABLES="$HOME/HypertritonAnalysis/DerivedTrees"
+HYPERML_FIGURES="$HOME/HypertritonAnalysis/Figures"
+HYPERML_MODELS="$HOME/HypertritonAnalysis/Models"
 
 BODY_2=0
 BODY_3=0
@@ -18,34 +19,33 @@ if [ "$1" == "3" ] || [ "$1" == "3body" ] || [ "$1" == "3Body" ]; then
       BODY_3=1
 fi
 
-# bisogna far funzionare il download
 
 if [ $BODY_2 -eq 1 ]; then    
       export HYPERML_DATA_2="$HYPERML_DATA/2Body"
       export HYPERML_TABLES_2="$HYPERML_TABLES/2Body"
+      export HYPERML_FIGURES_2="$HYPERML_FIGURES/2Body"
+      export HYPERML_MODELS_2="$HYPERML_MODELS/2Body"
 
       [ ! -d "$HYPERML_DATA_2" ] && mkdir -p $HYPERML_DATA_2
+      [ ! -d "$HYPERML_TABLES_2" ] && mkdir -p $HYPERML_TABLES_2
+      [ ! -d "$HYPERML_FIGURES_2" ] && mkdir -p $HYPERML_FIGURES_2
+      [ ! -d "$HYPERML_MODELS_2" ] && mkdir -p $HYPERML_MODELS_2
       [ ! -f "$HYPERML_DATA_2/HyperTritonTree_18q.root"  ] &&alien:/alice/cern.ch/user/f/fmazzasc/Trees/HyperTritonTree_18q.root  $HYPERML_DATA_2/HyperTritonTree_18q.root
       [ ! -f "$HYPERML_DATA_2/HyperTritonTree_18r.root"  ] &&alien:/alice/cern.ch/user/f/fmazzasc/Trees/HyperTritonTree_18r.root  $HYPERML_DATA_2/HyperTritonTree_18r.root
       [ ! -f "$HYPERML_DATA_2/HyperTritonTree_19d2.root" ] &&alien:/alice/cern.ch/user/f/fmazzasc/Trees/HyperTritonTree_19d2.root $HYPERML_DATA_2/HyperTritonTree_19d2.root
-
-      [ ! -d "$HYPERML_TABLES_2" ] && mkdir -p $HYPERML_TABLES_2
 fi
 
 if [ $BODY_3 -eq 1 ]; then    
       export HYPERML_DATA_3="$HYPERML_DATA/3Body"
       export HYPERML_TABLES_3="$HYPERML_TABLES/3Body"
+      export HYPERML_FIGURES_3="$HYPERML_FIGURES/3Body"
+      export HYPERML_MODELS_3="$HYPERML_MODELS/3Body"
 
       [ ! -d "$HYPERML_DATA_3" ] && mkdir -p $HYPERML_DATA_3
-      [ ! -f "$HYPERML_DATA_3/HyperTritonTree_18q.root"  ] && alien_cp alien:/alice/cern.ch/user/p/pfecchio/HypertritonAnalysis/Trees/HyperTritonTree_18q.root  $HYPERML_DATA_3/HyperTritonTree_18q.root
-      [ ! -f "$HYPERML_DATA_3/HyperTritonTree_18r.root"  ] && alien_cp alien:/alice/cern.ch/user/p/pfecchio/HypertritonAnalysis/Trees/HyperTritonTree_18r.root  $HYPERML_DATA_3/HyperTritonTree_18r.root
-      [ ! -f "$HYPERML_DATA_3/HyperTritonTree_19d2.root" ] && alien_cp alien:/alice/cern.ch/user/p/pfecchio/HypertritonAnalysis/Trees/HyperTritonTree_19d2.root $HYPERML_DATA_3/HyperTritonTree_19d2.root
-
       [ ! -d "$HYPERML_TABLES_3" ] && mkdir -p $HYPERML_TABLES_3
+      [ ! -d "$HYPERML_FIGURES_3" ] && mkdir -p $HYPERML_FIGURES_3
+      [ ! -d "$HYPERML_MODELS_3" ] && mkdir -p $HYPERML_MODELS_3
+      [ ! -f "$HYPERML_DATA_3/HyperTritonTree_18q.root"  ] && alien_cp alien:/alice/cern.ch/user/p/pfecchio/Trees/HyperTritonTree_18q.root  $HYPERML_DATA_3/HyperTritonTree_18q.root
+      [ ! -f "$HYPERML_DATA_3/HyperTritonTree_18r.root"  ] && alien_cp alien:/alice/cern.ch/user/p/pfecchio/Trees/HyperTritonTree_18r.root  $HYPERML_DATA_3/HyperTritonTree_18r.root
+      [ ! -f "$HYPERML_DATA_3/HyperTritonTree_19d2.root" ] && alien_cp alien:/alice/cern.ch/user/p/pfecchio/Trees/HyperTritonTree_19d2.root $HYPERML_DATA_3/HyperTritonTree_19d2.root
 fi
-
-[ ! -d "$HYPERML_UTILS" ] && mkdir -p $HYPERML_UTILS
-[ ! -f "$HYPERML_UTILS/EventCounter.root"  ] && alien_cp alien:/alice/cern.ch/user/p/pfecchio/HypertritonAnalysis/Utils/EventCounter.root  $HYPERML_UTILS/HyperTritonTree_18q.root
-[ ! -f "$HYPERML_UTILS/BlastWaveFits.root"  ] && alien_cp alien:/alice/cern.ch/user/p/pfecchio/HypertritonAnalysis/Utils/BlastWaveFits.root  $HYPERML_UTILS/BlastWaveFits.root
-
-
