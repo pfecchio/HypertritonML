66af50f8fee3030384145eae744cc35809eb4cf1
Francesco Mazzaschi <fmazzasc@cern.ch>
2019-08-29 21:23:56 +0200

    Add macro to filter background from  a small MC sample

---
+++ 2body/TreeGeneration/HyperTreeFatherBkgMC.cc
@@ -0,0 +1,66 @@
+#include <TChain.h>
+#include <TF1.h>
+#include <TFile.h>
+#include <TRandom3.h>
+#include <TTreeReader.h>
+#include <TTreeReaderArray.h>
+#include <TTreeReaderValue.h>
+
+#include "AliPID.h"
+#include "AliAnalysisTaskHyperTriton2He3piML.h"
+
+#include <vector>
+
+#include "../include/Common.h"
+#include "../include/Table.h"
+#include "../include/GenTable.h"
+
+void HyperTreeFatherBkgMC(TString name = "HyperTritonTree_19d2.root")
+{
+
+  char *dataDir{nullptr}, *tableDir{nullptr};
+  getDirs(dataDir, tableDir);
+
+  TChain mcChain("_default/fTreeV0");
+  mcChain.AddFile(Form("%s/%s", dataDir, name.Data()));
+
+  TTreeReader fReader(&mcChain);
+  TTreeReaderArray<RHyperTritonHe3pi> RHyperVec = {fReader, "RHyperTriton"};
+  TTreeReaderArray<SHyperTritonHe3pi> SHyperVec = {fReader, "SHyperTriton"};
+  TTreeReaderValue<RCollision> RColl = {fReader, "RCollision"};
+
+  TFile tableFile(Form("%s/BkgTable.root", tableDir), "RECREATE");
+  Table outputTable("BkgTable", "Bkg table");
+
+  while (fReader.Next())
+  {
+    std::vector<float> index;
+    for (auto &SHyper : SHyperVec)
+    {
+
+      int ind = SHyper.fRecoIndex;
+
+      if (ind >= 0)
+      {
+        index.push_back(ind);
+      }
+    }
+
+    float Rind = 0;
+    for (auto &RHyper : RHyperVec)
+    {
+      bool fake = false;
+      for (auto &i : index)
+      {
+        if (Rind == i)
+          fake = true;
+      }
+      if (fake == false)
+        outputTable.Fill(RHyper, *RColl);
+
+      Rind = Rind + 1;
+    }
+  }
+  tableFile.cd();
+  outputTable.Write();
+}
