f9b75c3458854fcfce128d060ea369a22447b465
Maximiliano Puccio <maximiliano.puccio@cern.ch>
2019-10-04 18:29:33 +0200

    Use custom defined efficiency ranges [TBT]

--- common/distribution_analysis.py
+++ common/distribution_analysis.py
@@ -30,6 +30,13 @@ resultsSysDir = os.environ['HYPERML_RESULTS_{}'.format(params['NBODY'])]
 var = '#it{ct}'
 unit = 'cm'
 
+rangesFile = resultsSysDir + '/' + params['FILE_PREFIX'] + '_effranges.yaml'
+with open(os.path.expandvars(rangesFile), 'r') as stream:
+    try:
+        ranges = yaml.full_load(stream)
+    except yaml.YAMLError as exc:
+        print(exc)
+
 file_name = resultsSysDir +  '/' + params['FILE_PREFIX'] + '_dist.root'
 distribution = TFile(file_name,'recreate')
 
@@ -56,43 +63,50 @@ for cclass in params['CENTRALITY_CLASS']:
   outDir.cd()
   h1PreselEff.Write("h1PreselEff")
   hRawCounts = []
-  raws = [[],[],[],[],[],[],[],[]]
-  trialNames = []
+  raws = []
+  errs = []
   for model in bkgModels:
-    for fix_eff in  params['BDT_EFFICIENCY']:
-      if (float(fix_eff) > 0.75 or float(fix_eff) < 0.6):
-        continue
-      h2RawCounts = resultFile.Get('{}/RawCounts{}_{}'.format(inDirName,fix_eff,model))
-      h1RawCounts = h2RawCounts.ProjectionY()
-      h1RawCounts.SetTitle(";#it{ct} (cm);dN/d#it{ct} (cm)^{-1}")
-      h1RawCounts.Divide(h1PreselEff)
-      h1RawCounts.Scale(1./float(fix_eff),'width')
-      outDir.cd()
-      for iBin in range(0,h1RawCounts.GetNbinsX()):
-        raws[iBin].append(h1RawCounts.GetBinContent(iBin + 1))
-      h1RawCounts.UseCurrentStyle()
-      h1RawCounts.Fit(expo,"MI")
-      h1RawCounts.Write('ctSpectra{}_{}'.format(fix_eff,model))
-      hRawCounts.append(h1RawCounts)
+    
+    h1RawCounts = h1PreselEff.Clone("best_{}".format(model))
+    h1RawCounts.Reset()
+    for iBin in range(0,h1RawCounts.GetNbinsX()):
+      h2RawCounts = resultFile.Get('{}/RawCounts{}_{}'.format(inDirName,ranges['BEST'][iBin],model))
+      h1RawCounts.SetBinContent(iBin + 1, h2RawCounts.GetBinContent(1, iBin + 1) / ranges['BEST'][iBin] / h1RawCounts.GetBinWidth(iBin + 1))
+      h1RawCounts.SetBinError(iBin + 1, h2RawCounts.GetBinError(1, iBin + 1) / ranges['BEST'][iBin] / h1RawCounts.GetBinWidth(iBin + 1))
+      raws.append([])
+      errs.append([])
+      for eff in np.arange(ranges['SCAN'][iBin][0],ranges['SCAN'][iBin][1],ranges['SCAN'][iBin][2]):
+        h2RawCounts = resultFile.Get('{}/RawCounts{}_{}'.format(inDirName,eff,model))
+        raws[iBin].append(h2RawCounts.GetBinContent(1, iBin + 1) / eff / h1RawCounts.GetBinWidth(iBin + 1))
+        errs[iBin].append(h2RawCounts.GetBinError(1, iBin + 1) / eff / h1RawCounts.GetBinWidth(iBin + 1))
 
-      cvDir.cd()
-      myCv = TCanvas("ctSpectraCv{}_{}".format(fix_eff,model))
-      pinfo2= TPaveText(0.5,0.5,0.91,0.9,"NDC")
-      pinfo2.SetBorderSize(0)
-      pinfo2.SetFillStyle(0)
-      pinfo2.SetTextAlign(30+3)
-      pinfo2.SetTextFont(42)
-      string ='ALICE Internal, Pb-Pb 2018 {}-{}%'.format(0,90)
-      pinfo2.AddText(string)
-      string='#tau = {:.0f} #pm {:.0f} ps '.format(expo.GetParameter(1),expo.GetParError(1))
-      pinfo2.AddText(string)  
-      if expo.GetNDF()is not 0:
-        string='#chi^{{2}} / NDF = {}'.format(expo.GetChisquare() / (expo.GetNDF()))
-      pinfo2.AddText(string)
-      h1RawCounts.Draw()
-      pinfo2.Draw()
-      cvDir.cd()
-      myCv.Write()
+
+    h1RawCounts.SetTitle(";#it{ct} (cm);dN/d#it{ct} (cm)^{-1}")
+    h1RawCounts.Divide(h1PreselEff)
+    outDir.cd()
+    h1RawCounts.UseCurrentStyle()
+    h1RawCounts.Fit(expo,"MI")
+    h1RawCounts.Write()
+    hRawCounts.append(h1RawCounts)
+
+    cvDir.cd()
+    myCv = TCanvas("ctSpectraCv_{}".format(model))
+    pinfo2= TPaveText(0.5,0.5,0.91,0.9,"NDC")
+    pinfo2.SetBorderSize(0)
+    pinfo2.SetFillStyle(0)
+    pinfo2.SetTextAlign(30+3)
+    pinfo2.SetTextFont(42)
+    string ='ALICE Internal, Pb-Pb 2018 {}-{}%'.format(0,90)
+    pinfo2.AddText(string)
+    string='#tau = {:.0f} #pm {:.0f} ps '.format(expo.GetParameter(1),expo.GetParError(1))
+    pinfo2.AddText(string)  
+    if expo.GetNDF()is not 0:
+      string='#chi^{{2}} / NDF = {}'.format(expo.GetChisquare() / (expo.GetNDF()))
+    pinfo2.AddText(string)
+    h1RawCounts.Draw()
+    pinfo2.Draw()
+    cvDir.cd()
+    myCv.Write()
 
   outDir.cd()
 
@@ -101,21 +115,15 @@ for cclass in params['CENTRALITY_CLASS']:
   tmpCt = hRawCounts[0].Clone("tmpCt")
   
   combinations = set()
-  flag = 0
   size = 100000
-  for _ in range(100000):
+  for _ in range(size):
     tmpCt.Reset()
     comboList=[]
-    if random.uniform(0,1) < 100 / size:
-      flag =  flag + 1
     for iBin in range(1, tmpCt.GetNbinsX() + 1):
-      index = random.randint(0,len(hRawCounts)-1)
-      rdmCt = hRawCounts[index]
-      # if flag == 1:
-      #   trialNames.append(hRawCounts[index].GetName())
+      index = random.randint(0,len(raws[iBin])-1)
       comboList.append(index)
-      tmpCt.SetBinContent(iBin, rdmCt.GetBinContent(iBin))
-      tmpCt.SetBinError(iBin, rdmCt.GetBinError(iBin))
+      tmpCt.SetBinContent(iBin, raws[iBin][index])
+      tmpCt.SetBinError(iBin, raws[iBin][index])
     combo = (x for x in comboList)
     if combo in combinations:
       continue
@@ -123,76 +131,12 @@ for cclass in params['CENTRALITY_CLASS']:
     tmpCt.Fit(expo,"MI")
     prob.Fill(expo.GetProb())
     if expo.GetChisquare() < 3 * expo.GetNDF():
-      syst.Fill(expo.GetParameter(1))
-    # if flag == 1:
-    #   myCv = TCanvas("ctSpectraCv")
-    #   pinfo2= TPaveText(0.5,0.6,0.91,0.9,"NDC")
-    #   pinfo2.SetBorderSize(0)
-    #   pinfo2.SetFillStyle(0)
-    #   pinfo2.SetTextAlign(30+3)
-    #   pinfo2.SetTextFont(42)
-    #   string ='ALICE Internal, Pb-Pb 2018 {}-{}%'.format(0,90)
-    #   pinfo2.AddText(string)
-    #   string='#tau = {:.0f} #pm {:.0f} ps '.format(expo.GetParameter(1),expo.GetParError(1))
-    #   pinfo2.AddText(string)  
-    #   if expo.GetNDF()is not 0:
-    #     string='#chi^{{2}} / NDF = {}'.format(expo.GetChisquare() / (expo.GetNDF()))
-    #   pinfo2.AddText(string)
-    #   tmpCt.Draw()
-    #   tmpCt.SetMarkerStyle(20)
-    #   tmpCt.SetMarkerColor(600)
-    #   tmpCt.SetLineColor(600)
-    #   pinfo2.Draw("x0")
-    #   tmpSyst = tmpCt.Clone("hSyst")
-    #   corSyst = tmpCt.Clone("hCorr")
-    #   tmpSyst.SetFillStyle(0)
-    #   corSyst.SetFillStyle(3345)
-    #   for iBin in range(1, tmpCt.GetNbinsX() + 1):
-    #     val = tmpSyst.GetBinContent(iBin)
-    #     tmpSyst.SetBinError(iBin, np.std(raws[iBin - 1]))
-    #     corSyst.SetBinError(iBin, 0.086 * val)
-    #   tmpSyst.Draw("e2same")
-    #   corSyst.Draw("e2same")
-    #   outDir.cd()
-    #   myCv.Write()
-    #   flag = 2
-  
-  myCv = TCanvas("ctSpectraCv")
-  pinfo2= TPaveText(0.5,0.6,0.91,0.9,"NDC")
-  pinfo2.SetBorderSize(0)
-  pinfo2.SetFillStyle(0)
-  pinfo2.SetTextAlign(30+3)
-  pinfo2.SetTextFont(42)
-  string ='ALICE Internal, Pb-Pb 2018 {}-{}%'.format(0,90)
-  pinfo2.AddText(string)
-  expo7 = hRawCounts[7].GetFunction('myexpo')
-  string='#tau = {:.0f} #pm {:.0f} ps '.format(expo7.GetParameter(1),expo7.GetParError(1))
-  pinfo2.AddText(string)  
-  if expo7.GetNDF()is not 0:
-    string='#chi^{{2}} / NDF = {}'.format(expo7.GetChisquare() / (expo7.GetNDF()))
-  pinfo2.AddText(string)
-  hRawCounts[7].Draw()
-  hRawCounts[7].SetMarkerStyle(20)
-  hRawCounts[7].SetMarkerColor(600)
-  hRawCounts[7].SetLineColor(600)
-  pinfo2.Draw("x0")
-  tmpSyst = hRawCounts[7].Clone("hSyst")
-  corSyst = hRawCounts[7].Clone("hCorr")
-  tmpSyst.SetFillStyle(0)
-  corSyst.SetFillStyle(3345)
-  for iBin in range(1, hRawCounts[7].GetNbinsX() + 1):
-    val = tmpSyst.GetBinContent(iBin)
-    tmpSyst.SetBinError(iBin, np.std(raws[iBin - 1]))
-    corSyst.SetBinError(iBin, 0.086 * val)
-  tmpSyst.Draw("e2same")
-  corSyst.Draw("e2same")
-  outDir.cd()
-  myCv.Write()
+      syst.Fill(expo.GetParameter(1))∂
 
   syst.SetFillColor(600)
   syst.SetFillStyle(3345)
   syst.Write()
   prob.Write()
-  print(trialNames)
+
 resultFile.Close()
 
