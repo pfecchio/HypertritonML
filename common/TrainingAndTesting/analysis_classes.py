ce7f21e55335b1b6d463d6d60f6f329865558ec3
Pietro Fecchio <pfecchio@cern.ch>
2020-07-06 16:41:28 +0200

    restore efficiency for 3body

--- common/TrainingAndTesting/analysis_classes.py
+++ common/TrainingAndTesting/analysis_classes.py
@@ -55,11 +55,10 @@ class TrainingAnalysis:
     def preselection_efficiency(self, cent_class, ct_bins, pt_bins, split_type, save=True):
         cent_cut = f'{cent_class[0]}<=centrality<={cent_class[1]}'
 
+        pt_bins = [0, 10]
+
         pres_histo = hau.h2_preselection_efficiency(pt_bins, ct_bins)
         gen_histo = hau.h2_generated(pt_bins, ct_bins)
-
-        fill_hist(pres_histo, self.df_signal.query(cent_cut)[['pt', 'ct']])
-        fill_hist(gen_histo, self.df_generated.query(cent_cut)[['pt', 'ct']])
 
         pres_histo.Divide(gen_histo)
 
