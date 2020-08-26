17952e915da8103f4ada94f79d21c24b97a645ca
fmazzasc <fmazzasc@cern.ch>
2020-08-26 11:06:54 +0200

    Add new feature in the analysis

--- 3body/TreesToTables/PrepareDataFrames.cc
+++ 3body/TreesToTables/PrepareDataFrames.cc
@@ -24,7 +24,7 @@ void PrepareDataFrames() {
   }
 
   ROOT::RDataFrame df("Hyp3O2",vecOfStrs);
-  std::vector<std::string> features{"dca_de","dca_pr","dca_pi","dca_de_sv","dca_pr_sv","dca_pi_sv","tpcClus_de","tpcClus_pr","tpcClus_pi","tpcNsig_de","tpcNsig_pr","tpcNsig_pi","dca_de_pr","dca_de_pi","dca_pr_pi","mppi_vert"};
+  std::vector<std::string> features{"dca_de","dca_pr","dca_pi","dca_de_sv","dca_pr_sv","dca_pi_sv","tpcClus_de","tpcClus_pr","tpcClus_pi","tpcNsig_de","tpcNsig_pr","tpcNsig_pi","dca_de_pr","dca_de_pi","dca_pr_pi","mppi_vert", "cosTheta_ProtonPiH"};
   auto filterDF = df.Filter("cosPA > 0 ");
   for (auto& f : features) {
     filterDF = filterDF.Define(f + "_f", Form("(float)%s",f.data()));
@@ -40,7 +40,7 @@ void PrepareDataFrames() {
   features.push_back("chi2");
 
   auto rHist = filterDF.Histo1D({"rHist",";r (cm); Entries", 200,0,200},"r");
-  filterDF.Snapshot("DataTable","../../../tmp/HypTableDataLS.root",features);
+  filterDF.Snapshot("DataTable","../../../tmp/HypTableData.root",features);
 
   TFile output("output.root","recreate");
   TCanvas rCv("rCv");
