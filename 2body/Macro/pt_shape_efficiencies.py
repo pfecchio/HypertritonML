3f8a33cd6f31b7be9a3bc03c440287be345ea3e3
Pietro Fecchio <pfecchio@cern.ch>
2020-12-10 10:24:03 +0100

    fix aghast histogram conversion

--- 2body/Macro/pt_shape_efficiencies.py
+++ 2body/Macro/pt_shape_efficiencies.py
@@ -7,32 +7,41 @@ import uproot
 from ROOT import TH1D, gROOT, TFile
 import aghast
 
-gROOT.LoadMacro("../TreeToTables/GenerateTableFromMC.cc")
+gROOT.LoadMacro('../TreeToTables/GenerateTableFromMC.cc')
 from ROOT import GenerateTableFromMC
 
-input_dir = os.environ['HYPERML_DATA_2']+ "/splines_trees"
-output_dir = os.environ['HYPERML_TABLES_2']+ "/splines_tables"
+input_dir = os.environ['HYPERML_DATA_2']
+output_dir = os.environ['HYPERML_TABLES_2']
 
-ct_bins = np.array([0,1,2,4,6,8,10,14,18,23,35],"d")
+ct_bins = np.array([0,1,2,4,6,8,10,14,18,23,35], 'd')
 
 
-pt_shape_list = ["bol", "mtexp", "bw"]
+pt_shape_list = ['bol', 'mtexp', 'bw']
 
 hist_list = []
 
 for pt_shape in pt_shape_list:
     GenerateTableFromMC(True, input_dir, output_dir, pt_shape)
-    rec = np.histogram(uproot.open(output_dir+"/SignalTable.root")["SignalTable"].array("ct"), bins=ct_bins)
-    sim = np.histogram(uproot.open(output_dir+"/SignalTable.root")["GenTable"].array("ct"), bins=ct_bins)
-    th1_rec = aghast.to_root(rec, "PreselEff_" + pt_shape)
-    th1_rec.SetTitle(";c#it{t} (cm);Preselection efficiency")
-    th1_sim = aghast.to_root(sim, "Sim_" + pt_shape)
+
+    numpy_rec = np.histogram(uproot.open(output_dir + '/SignalTable_20g7.root')['SignalTable'].array('ct'), bins=ct_bins)
+    numpy_sim = np.histogram(uproot.open(output_dir + '/SignalTable_20g7.root')['GenTable'].array('ct'), bins=ct_bins)
+
+    ghastly_rec = aghast.from_numpy(numpy_rec)
+    ghastly_sim = aghast.from_numpy(numpy_sim)
+
+    th1_rec = aghast.to_root(ghastly_rec, 'PreselEff_' + pt_shape)
+    th1_rec.SetTitle(';c#it{t} (cm);Preselection efficiency')
+
+    th1_sim = aghast.to_root(ghastly_sim, 'Sim_' + pt_shape)
+
     th1_rec.Divide(th1_sim)
     hist_list.append(th1_rec)
 
 
-tfile = TFile(os.environ['HYPERML_EFFICIENCIES_2'] + "/pt_shape_comparison", "recreate")
+tfile = TFile(os.environ['HYPERML_UTILS_2'] + '/pt_shape_comparison.root', 'recreate')
+
 for hist in hist_list:
     hist.Write()
+    
 tfile.Close()
 
